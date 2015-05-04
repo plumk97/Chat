824099abadbc79d0afcd67478e16adcfdfa25fad
货道网 <13923796186@163.com>
2015-05-04 14:11:55 +0800

    Initial Commit

---
+++ Chat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Chat
+//
+//  Created by 货道网 on 15/5/4.
+//  Copyright (c) 2015年 李铁柱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
